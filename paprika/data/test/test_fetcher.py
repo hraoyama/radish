4de7b416b102b755cfda8624ed9d6b1dbfa702ed
SweatOfRa <john.ryeman@gmail.com>
2019-07-18 22:48:37 +0100

    detail 2

--- paprika/data/test/test_fetcher.py
+++ paprika/data/test/test_fetcher.py
@@ -1,11 +1,13 @@
 import pytest
-from ..fetcher import *
+from paprika.data.fetcher import *
 
 def test_fetcher():
 
+    logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler()])
     data_fetcher = HistoricalDataFetcher()
-    start_time = datetime(2018, 2, 1)
-    end_time = datetime(2018, 2, 2)
-    df = data_fetcher.fetch_orderbook('mdb', 'ETF.LU0252634307.OrderBook', '1m', start_time, end_time)
+    start_time = datetime(2017, 5, 1)
+    end_time = datetime(2017, 6, 10)
+    meta = data_fetcher.get_meta_data("mdb")
+    df = data_fetcher.fetch_orderbook('mdb', 'EUX.FDAX201709', start_time=start_time, end_time=end_time)
     # print(data_fetcher.fetch_ohlcv(request))
     assert df.shape[0] > 100
