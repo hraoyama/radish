c70868ee993803e688c4e74abb88938a47c1b77a
SweatOfRa <john.ryeman@gmail.com>
2019-07-20 10:56:37 +0100

    added combined frequency/volume filter

--- paprika/data/feed_filter.py
+++ paprika/data/feed_filter.py
@@ -7,42 +7,69 @@ from abc import ABC
 print(os.getenv("RADISH_PATH"))
 print(os.getenv("PAPRIKA_PATH"))
 
-
 from enum import Enum
-from fetcher import HistoricalDataFetcher
-from fetcher import DataUploader
 
 
-class FreqFilter(ABC):
+# from fetcher import HistoricalDataFetcher
+# from fetcher import DataUploader
+
+
+class FilterType(Enum):
+    TIME = 'Time'
+    VOLUME = 'Volume'
+
+
+class FilterInterface(ABC):
+    pass
+
+
+class Filtration(object):
+    def __init__(self):
+        self.filters = []
+    
+    def add_filter(self, filter: FilterInterface):
+        self.filters.append(filter)
+    
+    def __str__(self):
+        return '\n'.join([str(f) for f in self.filters])
+
+
+class FreqFilter(FilterInterface):
     def __init__(self, period, length):
+        super().__init__()
         self.length = length
         self.period = period
-
-    def __str__(self):
-        return str(type(self.period)), str(self.period), str(type(self.length)), str(self.length)
     
+    def __str__(self):
+        type_of_length = "NoneType" if self.length is None else str(type(self.length))
+        return " ".join([str(type(self.period)), str(self.period), type_of_length, str(self.length)])
+
+
 class TimePeriod(Enum):
     DAY = 'D'
     WEEK = 'W'
+    WEEKLY = 0
     HOUR = 'H'
     MINUTE = 'M'
     SECOND = 'S'
     MS = 'MS'
     CONTINUOUS = ''
-    
+
+
 class TimeFreqFilter(FreqFilter):
-    def __init__(self, period, length = 1):
+    def __init__(self, period, length=None):
         assert isinstance(period, TimePeriod)
-        super(period, length)
+        if length is None:
+            assert period == TimePeriod.CONTINUOUS
+        super().__init__(period, length)
 
 
-class Filter:
-    
-    def __init__(self):
-        self.filter_dict = {}
-        
-        
 if __name__ == "__main__":
     a = TimeFreqFilter(TimePeriod.SECOND, 30)
-    print(a)
-    
+    b = TimeFreqFilter(TimePeriod.WEEK, 2)
+    c = TimeFreqFilter(TimePeriod.CONTINUOUS)
+    filt = Filtration()
+    filt.add_filter(a)
+    filt.add_filter(c)
+    filt.add_filter(b)
+    print(filt)
