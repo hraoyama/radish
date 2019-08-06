7e7c9d4546c215bd605491195e932e4b9017592f
SweatOfRa <john.ryeman@gmail.com>
2019-08-06 18:50:04 +0100

    added redis cache for the temporary feeds + extra comments

--- paprika/data/data_channel.py
+++ paprika/data/data_channel.py
@@ -3,15 +3,19 @@ import logging
 
 from arctic import Arctic
 from arctic import CHUNK_STORE
+import redis
 
 
 class DataChannel:
+    redis = redis.Redis('localhost')
+    
     @staticmethod
     def upload(data_frame: pd.DataFrame,
                table_name: str,
                is_overwrite: bool = True,
                arctic_source_name: str = 'feeds',
-               arctic_host: str = 'localhost'):
+               arctic_host: str = 'localhost',
+               put_in_redis=True):
         
         arctic = Arctic(arctic_host)
         if arctic_source_name not in arctic.list_libraries():
@@ -26,26 +30,39 @@ class DataChannel:
         else:
             library.append(table_name, data_frame)
         
-        logging.info(f"Uploaded {table_name} to {arctic_source_name} on {arctic_host}")
+        logging.info(f"Uploaded Arctic {table_name} to {arctic_source_name} on {arctic_host}")
+        
+        if put_in_redis:
+            DataChannel.redis.set(table_name, data_frame.to_msgpack(compress='blosc'))
+            logging.info(f"Uploaded Redis {table_name} on localhost")
+        
         return arctic_host, arctic_source_name, table_name
     
     @staticmethod
     def download(table_name: str,
                  arctic_source_name: str = 'feeds',
-                 arctic_host: str = 'localhost'):
+                 arctic_host: str = 'localhost',
+                 use_redis=True):
         
-        arctic = Arctic(arctic_host)
-        assert arctic_source_name in arctic.list_libraries()
-        
-        library = arctic[arctic_source_name]
-        logging.info(f"Read {table_name} inside {arctic_source_name} on {arctic_host}.")
-        return library.read(table_name)
+        msg = DataChannel.redis.get(table_name) if use_redis else None
+        if msg:
+            logging.debug(f'Load Redis cache for {table_name}')
+            return pd.read_msgpack(msg)
+        else:
+            logging.debug(f'Load Arctic cache for {table_name}')
+            arctic = Arctic(arctic_host)
+            assert arctic_source_name in arctic.list_libraries()
+            
+            library = arctic[arctic_source_name]
+            logging.info(f"Read {table_name} inside {arctic_source_name} on {arctic_host}.")
+            return library.read(table_name)
     
     @staticmethod
     def delete_table(table_name: str, arctic_source_name: str = 'feeds', arctic_host: str = 'localhost'):
         arctic = Arctic(arctic_host)
         assert arctic_source_name in arctic.list_libraries()
         library = arctic[arctic_source_name]
+        DataChannel.redis.delete(table_name)
         return library.delete(table_name)
     
     @staticmethod
@@ -65,4 +82,5 @@ class DataChannel:
             arctic.initialize_library(arctic_source_name, lib_type=CHUNK_STORE)
         library = arctic[arctic_source_name]
         for table_name in library.list_symbols():
+            DataChannel.redis.delete(table_name)
             library.delete(table_name)
--- paprika/signals/pair_spread_ols_signal.py
+++ paprika/signals/pair_spread_ols_signal.py
@@ -42,10 +42,10 @@ class PairSpread(FeedSubscriber):
         data = events[0][1]
         
         if any(data.ISIN == self.get_parameter("Y")):
-            self.num_y_obs = 1 if not self.num_y_obs else 0  # only keep the last observation
+            self.num_y_obs = 1 # only keep the last observation
             self.last_y = data.loc[data.ISIN == self.get_parameter("Y"), "Price"][-1]
         if any(data.ISIN == self.get_parameter("X")):
-            self.num_x_obs = 1 if not self.num_x_obs else 0
+            self.num_x_obs = 1
             self.last_x = data.loc[data.ISIN == self.get_parameter("X"), "Price"][-1]
         
         if self.num_x_obs == self.num_y_obs and self.num_x_obs > 0:
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     print(pair_signal_2.parameters)
     
     
-    # let's use the running historical standard deviation
+    # let's use the rolling historical standard deviation
     pair_signal_3 = PairSpread(Y="FBTP201709", X="FBTS201709",
                                BETA=1.2, SD_FACTOR=2.0, NUM_OBS=20.0)
     pair_signal_3.add_filtration(Filtration(TimeFreqFilter(TimePeriod.SECOND, 600)))
@@ -147,7 +147,7 @@ if __name__ == "__main__":
     print(pair_signal_3.get_parameter("BETA_HISTORY"))
     print(pair_signal_3.parameters)
     
-    # let's use the running historical standard deviation and some adaptive beta
+    # let's use the rolling historical standard deviation and some adaptive beta
     pair_signal_4 = PairSpread(Y="FBTP201709", X="FBTS201709",
                                BETA=1.2, SD_FACTOR=1.0, NUM_OBS=20.0, BETA_UPDATE_FACTOR=1.05)
     pair_signal_4.add_filtration(Filtration(TimeFreqFilter(TimePeriod.SECOND, 600)))
@@ -157,5 +157,21 @@ if __name__ == "__main__":
     print(pair_signal_4.get_parameter("BETA_HISTORY"))
     print(pair_signal_4.parameters)
     
+    # check that we don't get a speed penalty for accidentally creating a duplicated feed
+    # set up the pairs data you will be looking at
+    pairs_feed_duplicate = Feed('PAIRS_FEED_2', datetime(2017, 7, 1), datetime(2017, 7, 28))
+    pairs_feed_duplicate.set_feed(["EUX.FBTP201709", "EUX.FBTS201709"], DataType.TRADES)
+
+    # how much data did we actually get?
+    pp(pairs_feed_duplicate.shape)
+
+    pair_signal_5 = PairSpread(Y="FBTP201709", X="FBTS201709",
+                               BETA=1.2, SD_FACTOR=1.0, NUM_OBS=40.0)
+    pair_signal_5.add_filtration(Filtration(TimeFreqFilter(TimePeriod.SECOND, 600)))
+    pairs_feed_duplicate.add_subscriber(pair_signal_5)
+
+    # execute the signal
+    pair_signal_5.run()
+
     # clear out your used feeds
     DataChannel.clear_all_feeds()
