ee11011d4c3e9a5352c2a1970a8abca310be8150
Gr1ngo <islam.utyagulov@gmail.com>
2019-09-03 22:28:08 +0100

    Goldie Boys adding test file

--- paprika/signals/gold_cointegration.py
+++ paprika/signals/gold_cointegration.py
@@ -16,13 +16,16 @@ class GoldSpread(FeedSubscriber):
         self.entry = self.get_parameter("ENTRY")
         self.exit = self.get_parameter("EXIT")
         self.positions = pd.DataFrame()
+        self.prices = pd.DataFrame()
 
     def handle_event(self, events: List[Tuple[DataType, pd.DataFrame]]):
 
         super().handle_event(events)
         data = events[0][1]
-        z_score = (data['GLD'][0] - self.beta*data['GDX'][0] - self.mean) / self.sigma
         timestamp = data.index[0]
+        self.prices = self.prices.append(pd.DataFrame({'DateTime': [timestamp], 'GLD': data['GLD'][0], 'GDX': data['GDX'][0]}))
+        z_score = (data['GLD'][0] - self.beta*data['GDX'][0] - self.mean) / self.sigma
+
         if z_score <= -self.entry:
             self.positions = self.positions.append(pd.DataFrame({'DateTime': [timestamp], 'GLD': [1.0], 'GDX': [-1.0]}))
         elif z_score >= self.entry:
--- paprika/signals/tests/test_gold_cointegration.py
+++ paprika/signals/tests/test_gold_cointegration.py
@@ -1,40 +1,14 @@
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-
 from datetime import datetime
 from paprika.data.data_type import DataType
-from paprika.data.data_channel import DataChannel
 from paprika.data.feed import Feed
-from paprika.data.feed_filter import TimeFreqFilter, Filtration, TimePeriod
 from paprika.signals.gold_cointegration import GoldSpread
 from paprika.utils import utils
+import matplotlib.pyplot as plt
 
-PATH = r'../../../resources/data/'
 
+def test_gold_cointegration():
 
-def main():
-    tckr1 = 'GLD'
-    tckr2 = 'GDX'
-    ts1 = pd.read_excel(os.path.join(PATH, '{}.xls'.format(tckr1)), usecols=[0, 6])
-    ts1 = ts1.sort_values(by=['Date'])
-    ts1 = ts1.reset_index(drop=True)
-
-    ts2 = pd.read_excel(os.path.join(PATH, '{}.xls'.format(tckr2)), usecols=[0, 6])
-    ts2 = ts2.sort_values(by=['Date'])
-    ts2 = ts2.reset_index(drop=True)
-
-    ts = pd.merge(ts1, ts2, how='inner', on=['Date'])
-    ts.columns = ['date', 'GLD', 'GDX']
-    ts.set_index('date', inplace=True)
-    
-    DataType.extend("EOD_PRICE")
-    
-    table_name = DataChannel.name_to_data_type("GOLD_GDX", DataType.EOD_PRICE)
-    DataChannel.upload(ts, table_name)
-    # following stores it in DB permanently - only do this if you are sure you need to keep this data
-    # DataChannel.upload_to_permanent(table_name)
-
+    tckr1, tckr2 = 'GLD', 'GDX'
     gold_feed = Feed('GOLD_FEED', datetime(1950, 7, 1), datetime(2050, 1, 1))
     gold_feed.set_feed("GOLD_GDX", DataType.EOD_PRICE)
 
@@ -66,10 +40,3 @@ def main():
     sharp_cost_adj_test = utils.sharpe(port_return_minus_costs[train_idx:], 252)
     print("Sharpe ratio on the train {:.2f} and test {:.2f} sets respectively "
           "after adjusting for transaction costs.".format(sharp_cost_adj_tr, sharp_cost_adj_test))
-
-    # clear out any feeds in the cache
-    DataChannel.clear_all_feeds()
-
-
-if __name__ == "__main__":
-    main()
