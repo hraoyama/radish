5cef68387b4099f42a346c24193566ae085e3f9b
xia <xia.alex.fang@gmail.com>
2019-11-06 23:31:28 +0000

    Add fetch_price for Fetcher

--- paprika/data/fetcher.py
+++ paprika/data/fetcher.py
@@ -18,35 +18,37 @@ from arctic import CHUNK_STORE
 from arctic.date import DateRange
 
 from .data_type import DataType
+from paprika.data.constants import OrderBookColumnName
+from paprika.data.constants import TradeColumnName
 
 
 class HistoricalDataFetcher:
     DATETIME_FORMAT = "%Y%m%d%H%M%S"
     DATE_FORMAT = "%Y%m%d"
-    
+
     @staticmethod
     def generate_pattern_list(exchanges: List[str] = None,
                               instruments: List[str] = None,
                               data_types: List[DataType] = None) -> List:
-        
+
         exchanges = ["^.*"] if exchanges is None else list(map(lambda x: ".*" + x.upper().strip() + ".*", exchanges))
         instruments = [".*"] if instruments is None else list(
             map(lambda x: ".*" + x.upper().strip() + ".*", instruments))
         data_types = [str(DataType.ORDERBOOK) + "$"] if data_types is None else list(
             map(lambda x: str(x) + "$", data_types))
-        
+
         return list(map(lambda tup: ".".join(tup), itertools.product(exchanges, instruments, data_types)))
-    
+
     @staticmethod
     def generate_simple_pattern_list(descs: List[str] = None, data_type: DataType = DataType.ORDERBOOK) -> List:
         descs = ["^.*"] if descs is None else list(map(lambda x: ".*" + x.upper().strip() + ".*", descs))
         data_type_pattern = [str(data_type) + "$"]
         return list(map(lambda tup: ".".join(tup), itertools.product(descs, data_type_pattern)))
-    
+
     @staticmethod
     def _scrub_pattern_from_string(patterned_string: str):
         return patterned_string.replace("^", "").replace(".*", "").replace("$", "")
-    
+
     def __init__(self,
                  arctic_source_name='mdb',
                  arctic_host='localhost',
@@ -57,7 +59,7 @@ class HistoricalDataFetcher:
             self.arctic.initialize_library(arctic_source_name, lib_type=CHUNK_STORE)
         self.library = self.arctic[arctic_source_name]
         self.available_feeds = self.library.list_symbols()
-    
+
     def fetch_from_pattern_list(self,
                                 pattern_list_str: List[str],
                                 start_time: Optional[Union[int, datetime]] = None,
@@ -66,7 +68,7 @@ class HistoricalDataFetcher:
                                 field_columns: List[str] = None,
                                 between_times=None,
                                 shared_index=False):
-        
+
         pattern_list = [re.compile(x) for x in pattern_list_str]
         symbols_in_arctic = [symbol_match for symbol_match in self.available_feeds for plist in pattern_list if
                              plist.match(symbol_match)]
@@ -82,7 +84,7 @@ class HistoricalDataFetcher:
                     lambda df1, df2: pd.concat([df1, df2], ignore_index=False, sort=True), dfs)
             else:
                 return [], None
-        
+
         dfs = list(map(lambda x:
                        self.fetch(symbol=x, fields=field_columns, start_time=start_time, end_time=end_time,
                                   add_symbol=add_symbol),
@@ -95,26 +97,26 @@ class HistoricalDataFetcher:
                 ignore_index=False, sort=True), dfs))
 
         df_all = df_all.between_time(between_times[0], between_times[1]) if between_times is not None else df_all
-        
+
         # dfAll = functools.reduce(lambda df1, df2: pd.merge(df1, df2, how='outer', left_index=True, right_index=True),dfs)
         # dfAll.sort_index(inplace=True)
-        
+
         return symbols_in_arctic, df_all
-    
+
     def fetch(self,
               symbol: str,
               start_time: Optional[Union[int, datetime]] = None,
               end_time: Optional[Union[int, datetime]] = None,
               add_symbol: bool = True,
               fields: List[str] = None):
-        
+
         if fields is None:
             fields = []
         if start_time is int:
             start_time = millis_to_datetime(start_time)
         if end_time is int:
             end_time = millis_to_datetime(end_time)
-        
+
         key = symbol
         if start_time and end_time:
             redis_key = key + \
@@ -134,30 +136,31 @@ class HistoricalDataFetcher:
                         ".".join(map(str, fields))
 
         msg = self.redis.get(redis_key)
-        
+
         if msg:
             logging.debug(f'Load Redis cache for {redis_key}')
             return pd.read_msgpack(msg)
         else:
             logging.debug(f'Cache not existent for {redis_key}. Read from external source.')
-            
+
             df_result = None
             try:
                 df_result = self.library.read(symbol=key, chunk_range=DateRange(start_time, end_time), columns=fields)
+                df_result = df_result.loc[~df_result.index.duplicated(keep='first')]
                 if add_symbol:
                     df_result['Symbol'] = symbol
             except exceptions.NoDataFoundException as ndf:
                 logging.error(str(ndf))
                 return None
-            
+
             self.redis.set(redis_key, df_result.to_msgpack(compress='blosc'))
-            
+
             return df_result
-    
+
     def get_meta_data(self):
         return dict(zip(self.library.list_symbols(),
                         [self.library.read_metadata(symbol) for symbol in self.library.list_symbols()]))
-    
+
     def _get_redis_table(self, table_name):
         msg = self.redis.get(table_name)
         if msg:
@@ -165,7 +168,7 @@ class HistoricalDataFetcher:
             return pd.read_msgpack(msg)
         else:
             return None
-    
+
     def fetch_orderbook(self,
                         symbol: str,
                         start_time: Optional[Union[int, datetime]] = None,
@@ -173,3 +176,46 @@ class HistoricalDataFetcher:
                         add_symbol: bool = True,
                         fields: List[str] = None):
         return self.fetch(f'{symbol}.OrderBook', start_time, end_time, add_symbol, fields)
+
+    def fetch_price(self,
+                    symbol: str,
+                    timestamp: Optional[Union[int, datetime]],
+                    start_time: Optional[Union[int, datetime]] = None,
+                    end_time: Optional[Union[int, datetime]] = None,
+                    add_symbol: bool = True,
+                    fields: List[str] = None):
+        price = self.calc_price_from_orderbook(symbol, timestamp, start_time, end_time, add_symbol, fields)
+        if price:
+            return price
+        else:
+            return self.calc_price_from_trade(symbol, timestamp, start_time, end_time, add_symbol, fields)
+
+    def calc_price_from_trade(self,
+                              symbol: str,
+                              timestamp: Optional[Union[int, datetime]],
+                              start_time: Optional[Union[int, datetime]] = None,
+                              end_time: Optional[Union[int, datetime]] = None,
+                              add_symbol: bool = True,
+                              fields: List[str] = None):
+        df = self.fetch(f'{symbol}.Trade', start_time, end_time, add_symbol, fields)
+        if df is not None:
+            df_timestamp = df.loc[df.index[df.index.get_loc(timestamp, method='nearest')]]
+            return df_timestamp[TradeColumnName.Price]
+        else:
+            return None
+
+    def calc_price_from_orderbook(self,
+                                  symbol: str,
+                                  timestamp: Optional[Union[int, datetime]],
+                                  start_time: Optional[Union[int, datetime]] = None,
+                                  end_time: Optional[Union[int, datetime]] = None,
+                                  add_symbol: bool = True,
+                                  fields: List[str] = None):
+        df = self.fetch(f'{symbol}.OrderBook', start_time, end_time, add_symbol, fields)
+        if df is not None:
+            df_timestamp = df.loc[df.index[df.index.get_loc(timestamp, method='nearest')]]
+            bid1 = df_timestamp[OrderBookColumnName.Bid_Px_Lev_0]
+            ask1 = df_timestamp[OrderBookColumnName.Ask_Px_Lev_0]
+            return (bid1 + ask1) / 2.0
+        else:
+            return None
