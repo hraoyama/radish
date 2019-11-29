09a6f8eee63c1bcbcb2dbec79284e40fd66a5f65
xia <xia.alex.fang@gmail.com>
2019-11-29 13:00:32 +0000

    Update Alpha

--- paprika/alpha/alpha/alpha_01.py
+++ paprika/alpha/alpha/alpha_01.py
@@ -3,9 +3,12 @@ from paprika.alpha.utils import *
 from paprika.data.data_processor import DataProcessor
 
 
-def alpha_3(dp: DataProcessor):
-    return - correlation(rank(dp.open), rank(dp.volume), 10)
+def alpha_3(dp: DataProcessor, period=10):
+    return - correlation(rank(dp.open), rank(dp.volume), period)
 
 
-def alpha_4(dp: DataProcessor):
-    return - ts_rank(rank(dp.low), 9)
+alpha_3.info = "- correlation(rank(dp.open), rank(dp.volume), period)"
+
+
+def alpha_4(dp: DataProcessor, period=9):
+    return - ts_rank(rank(dp.low), period)
--- paprika/alpha/test/test_alpha.py
+++ paprika/alpha/test/test_alpha.py
@@ -6,20 +6,26 @@ from paprika.data.data_channel import DataChannel
 
 
 def test_alpha():
-    symbols = DataChannel.check_register(['MTA.*Trade'], False)
-    start = datetime(2017, 6, 1, 8, 15, 0)
+    symbols = DataChannel.check_register(["\w*.\w*.Trade"], False)
+    start = datetime(2010, 6, 1, 8, 15, 0)
     end = datetime(2019, 6, 1, 8, 15, 0)
-    time_filter = TimeFreqFilter(TimePeriod.MINUTE, 15, starting=datetime(2017, 6, 1, 8, 15, 0))
+    time_filter = TimeFreqFilter(TimePeriod.HOUR, 1)
+    dp = DataProcessor(symbols[0:2]).index(start, end)
+    dp.ohlcv(time_filter, inplace=True)
 
-    dp = DataProcessor(symbols[0:10], 'ohlcv', time_filter).index(start, end)
+    symbols2 = DataChannel.check_register(['\w*.\w*.EOD'], False)
+    time_filter2 = TimeFreqFilter(TimePeriod.WEEK, 1)
+    dp2 = DataProcessor(symbols2[0:2])
+    dp2.ohlcv(time_filter2, inplace=True)
 
-    alpha = Alpha(['alpha_\w*'], dp)
-    print(alpha.list_alpha())
+    alpha = Alpha(dp2)
     print(alpha.list_alpha_universe())
-
-    def alpha_5(dp):
-        return dp.close
-
-    alpha.add_alpha(alpha_5)
+    print(alpha.alpha_info('alpha_3'))
+    alpha.calc_alpha(['alpha_\w*'])
     print(alpha.list_alpha())
 
+    def alpha_5(dp, g):
+        return dp.close * g
+
+    alpha.add_alpha(alpha_5, 3)
+    print(alpha.list_alpha())
