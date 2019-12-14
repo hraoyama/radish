cecc4c58ee215826d087dfb06cee3bd5e635d926
xia <xia.alex.fang@gmail.com>
2019-12-14 23:10:02 +0000

    Update Alpha class

--- paprika/alpha/test/test_alpha.py
+++ paprika/alpha/test/test_alpha.py
@@ -3,25 +3,22 @@ from paprika.alpha.base import Alpha
 from paprika.data.data_processor import DataProcessor
 from paprika.data.feed_filter import TimeFreqFilter, TimePeriod
 from paprika.data.data_channel import DataChannel
+from paprika.data.data_type import DataType
 
 
 def test_alpha():
-    # symbols = DataChannel.check_register(["\w*.\w*.Trade"], False)
-    # start = datetime(2010, 6, 1, 8, 15, 0)
-    # end = datetime(2019, 6, 1, 8, 15, 0)
-    # time_filter = TimeFreqFilter(TimePeriod.HOUR, 1)
-    # dp = DataProcessor(symbols[0:2]).index(start, end)
-    # dp.ohlcv(time_filter, inplace=True)
 
-    symbols2 = DataChannel.check_register(['SP500.\w*.EOD'], False)
-    time_filter2 = TimeFreqFilter(TimePeriod.WEEK, 1)
-    dp2 = DataProcessor(symbols2[0:20])
-    dp2.ohlcv(time_filter2, inplace=True)
+    symbols = DataChannel.check_register(['.*.Candle'])
+    data = DataChannel.fetch(['SP500.A.*'], data_type=DataType.CANDLE)
+    time_filter = TimeFreqFilter(TimePeriod.WEEK, 1)
+    dp = DataProcessor(data)
+    dp.ohlcv(time_filter, inplace=True)
 
-    alpha = Alpha(dp2)
+    alpha = Alpha(dp)
     print(alpha.list_alpha_universe())
     print(alpha.alpha_info('alpha_3'))
-    alpha.calc_alpha(['alpha_\w*'])
+    alpha.calc_alpha(['alpha_.*'])
+    print(alpha['alpha_3'])
     print(alpha.list_alpha())
 
     def alpha_5(dp, g):
@@ -30,4 +27,7 @@ def test_alpha():
     alpha.add_alpha(alpha_5, 3)
     print(alpha.list_alpha())
 
-    alpha.analyze(symbols2[0])
+    corr_matrix = alpha.corr_between_alphas()
+    return_alpha_corr = alpha.corr_between_alpha_and_return()
+    # alpha.analyze_whole(predict_period=1)
+    print('ok')
